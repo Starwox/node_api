935882e639f2de059410f8adcdd9d11976937c04
Rezzoug Yannis <rezzoug.yannis@gmail.com>
2023-10-10 15:48:56 +0200

    adding parkings route for example

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const parkings = require('./parkings.json');
 const app = express();
-app.get('/parking', (req,res) => { res.send("Liste des parkings") })
+app.get('/parkings', (req,res) => {    
+    res.status(200).json(parkings)
+});
 app.listen(8080, () => {    console.log("Serveur à l'écoute")})
+app.get('/parkings/:id', (req,res) => {    
+    const id = parseInt(req.params.id)    
+    const parking = parkings.find(parking => parking.id === id)   
+    res.status(200).json(parking)
+})
